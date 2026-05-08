e3cbc700a73ef06a8c1c5289ad45e16449646efa
Vishesh Verma <visheshverma250703@gmail.com>
2026-05-09 00:53:43 +0530

    made changes in db url

--- backend/src/db/connect.ts
+++ backend/src/db/connect.ts
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 export async function connectDb() {
-    try {
-        await mongoose.connect("***REMOVED***");
-        console.log("listening on port 3000")
-    } catch (error) {
-        console.log("there is some error", error)
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI!);
+    console.log("listening on port 3000");
+  } catch (error) {
+    console.log("there is some error", error);
+  }
 }
